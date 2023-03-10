32e9b74e2382ec2c51d882117a26a5474045ad98
Haruxe <haruxe@proton.me>
2023-03-09 23:08:38 -0800

    basic menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Email } from "styled-icons/evaicons-solid";
 import { Check, Twitter } from "styled-icons/remix-fill";
 import { motion } from "framer-motion";
@@ -9,12 +9,14 @@ import { SignOut } from "styled-icons/fluentui-system-filled";
 //import { slide as Menu } from "react-burger-menu";
 // @ts-ignore
 function Navbar({ connected, address, connectHandler, disconnectHandler }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
   return (
     <>
       <nav className="mx-auto p-2 fixed right-0 left-0 bg-[#ffffff] backdrop-blur-xl bg-opacity-70 z-20">
         <div className="max-w-[1400px] mx-auto relative">
           <div className="flex">
-            <div className="absolute top-2 left-2 space-x-3 my-auto">
+            <div className="absolute top-2 left-2 space-x-3 my-auto invisible sm:visible">
               <a
                 href="https://twitter.com/web3sg"
                 target="_blank"
@@ -44,7 +46,15 @@ function Navbar({ connected, address, connectHandler, disconnectHandler }) {
                 <FacebookCircle className="w-5" />
               </a>
             </div>
-            <div className="absolute top-2 right-2 space-x-3 my-auto md:text-sm text-xs ">
+            <div className="absolute top-2 left-2 space-x-3 my-auto flex sm:hidden">
+              <button
+                className="h-8 w-8 my-auto border-2 border-black rounded flex flex-col justify-center items-center group"
+                onClick={() => setIsOpen(!isOpen)}
+              ></button>
+            </div>
+            < />
+
+            <div className="absolute top-2 right-2 space-x-3 my-auto md:text-sm text-sm invisible sm:visible">
               {!connected ? (
                 <button
                   className="select-none font-bold cursor-pointer font-gilroy-bold tracking-widest text-white px-2 rounded-sm py-1 bg-blue-500"
@@ -72,6 +82,7 @@ function Navbar({ connected, address, connectHandler, disconnectHandler }) {
                 </div>
               )}
             </div>
+
             <div className="mx-auto cursor-pointer w-[12rem] md:w-[16rem] my-auto pt-1">
               <Link href="/">
                 {/* <h1 className="md:text-4xl text-2xl select-none font-bold cursor-pointer font-gilroy tracking-widest text-white px-2 rounded-md py-1 bg-black hover:bg-[#303030]">
@@ -87,7 +98,8 @@ function Navbar({ connected, address, connectHandler, disconnectHandler }) {
               </Link>
             </div>
           </div>
-          <div className="mx-auto flex mt-3 mb-1 sm:w-[800px] place-content-between font-bold text-sm sm:text-lg">
+
+          <div className="mx-auto mt-3 mb-1 md:w-[800px] place-content-between font-bold text-sm sm:text-lg hidden sm:flex">
             <Link className="cursor-pointer" href="/">
               <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
                 <h1>Home</h1>
@@ -120,7 +132,47 @@ function Navbar({ connected, address, connectHandler, disconnectHandler }) {
             </Link>
           </div>
         </div>
+        {isOpen && (
+          <motion.div
+            className="font-bold space-y-3 p-2 mt-3"
+            animate={{ x: 1 }}
+            initial={{ x: -40 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Link className="cursor-pointer" href="/">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>Home</h1>
+              </motion.div>
+            </Link>
+            <Link className="cursor-pointer" href="/genesis">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>Genesis Pass</h1>{" "}
+              </motion.div>
+            </Link>
+            <Link className="cursor-pointer" href="/application">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>Application</h1>{" "}
+              </motion.div>
+            </Link>
+            <Link className="cursor-pointer" href="/about">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>About</h1>
+              </motion.div>
+            </Link>
+            <Link className="cursor-pointer" href="/partners">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>Partners</h1>
+              </motion.div>
+            </Link>
+            <Link className="cursor-pointer" href="/events">
+              <motion.div className="cursor-pointer rounded-lg px-3 py-1 hover:bg-[#7b7b7b15]">
+                <h1>Events</h1>
+              </motion.div>
+            </Link>
+          </motion.div>
+        )}
       </nav>
+
       <div className="pb-[8rem]" />
     </>
   );
